971fc9be0a9cea94b1aab7008a8f60db361da3ef
Handika743 <handikasanjaya743@gmail.com>
2025-05-05 17:34:33 +0700

    modif middleware

--- middleware.js
+++ middleware.js
@@ -5,16 +5,18 @@ export async function middleware(req) {
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
+    cookieName: "authjs.session-token", // WAJIB di v5
   });
 
   if (!token) {
-    return NextResponse.redirect(new URL("/", req.url));
-    // console.log(token);
+    console.log("⛔️ Belum login, redirect ke /login");
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
+  console.log("✅ Token ditemukan:", token);
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/menu/:path*"], // kalau kamu punya sub-route
+  matcher: ["/menu", "/menu/:path*"],
 };
